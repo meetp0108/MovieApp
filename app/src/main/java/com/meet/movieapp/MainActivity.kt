dde5984046d52d58059ce226ca28ab0eae4147a3
Meetkumar Patel <mpate959@my.centennialcollege.ca>
2025-01-17 02:19:19 -0500

    Using LazyColumn and Making Each Row Clickable

--- app/src/main/java/com/meet/movieapp/MainActivity.kt
+++ app/src/main/java/com/meet/movieapp/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.meet.movieapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon // Correct Icon import
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.meet.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +42,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainContent()
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        Scaffold(topBar = {
+            TopAppBar(
+                title = { Text(text = "Movie") },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+            )
+        })
+
+        { paddingValues ->
+            Surface(
+                modifier = Modifier.padding(paddingValues)
+                // color = MaterialTheme.colorScheme.primaryContainer
+            ) {
+                content()
+            }
 
         }
     }
@@ -24,25 +76,64 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun MainContent(){
-
-    Surface (color = MaterialTheme.colorScheme.background){
-        Text(text = "Hello")
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Happiness",
+        "Life",
+        "Be..",
+        "New Year",
+        "Hola"
+    )
+) {
+    LazyColumn {
+        items(items = movieList) {
+            MovieRow(movie = it){movie ->
+                Log.d("TAG", "MainContent: $movie")
+            }
+        }
     }
-
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyApp(content: @Composable () -> Unit) {
-    MovieAppTheme {
-        content()
+fun MovieRow(movie: String,onItemClick : (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClick(movie)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
     }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyApp{
+    MyApp {
         MainContent()
     }
 }
